c5d786f52c22062a3d31cc3fc4f286cfd08b8ba9
ahinnrix <113396585+ahinnrix@users.noreply.github.com>
2024-05-07 19:56:19 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from character import character
+
+music_man = Character(name ="Music_Man", stamina =50, action = 'walk')
+tiny_dancer = Character(name = "Tiny_Dancer", stamina =50, action = 'walk')
